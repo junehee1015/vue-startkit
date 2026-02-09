69eec3ad1916000ab88f1488c8a2859e84e4d975
Juny <junehee1015@iabacus.co.kr>
2026-02-09 18:12:50 +0900

    로그인 zod 이메일 부분 수정

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -19,7 +19,7 @@ const authStore = useAuthStore()
 
 const validationSchema = toTypedSchema(
   z.object({
-    email: z.string().min(1, '아이디를 입력해주세요.').email('올바른 이메일 형식이 아닙니다.'),
+    email: z.email('올바른 이메일 형식이 아닙니다.').min(1, '아이디를 입력해주세요.'),
     password: z.string('비밀번호를 입력해주세요.').min(1, '비밀번호를 입력해주세요.'),
     rememberMe: z.boolean().optional(),
   }),
